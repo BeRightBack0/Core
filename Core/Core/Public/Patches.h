7c23680a595727852b693eb44baf2ed5b896a3e1
PongoDev <90353497+PongooDev@users.noreply.github.com>
2026-07-23 14:49:50 +0100

    #75

--- Core/Core/Public/Patches.h
+++ Core/Core/Public/Patches.h
@@ -12,7 +12,20 @@ public:
 			Log("Patched: " + std::to_string(WidgetCrashPatch - ImageBase) + " with RetNullptr");
 		}
 
-		uintptr_t VivoxConnectPatch = Memcury::Scanner::FindStringRef(L"FVivox::Connect").FindFunctionStart().Get();
+		uintptr_t PrevVivoxInit = 0;
+		for (int RefNum = 0; RefNum < 2; RefNum++) {
+			auto InitRef = Memcury::Scanner::FindStringRef(L"FVivox::Initialize failure: %s (%i)", false, RefNum);
+			uintptr_t VivoxInitPatch = InitRef.IsValid() ? InitRef.FindFunctionStart().Get() : 0;
+
+			if (VivoxInitPatch && VivoxInitPatch != PrevVivoxInit) {
+				PrevVivoxInit = VivoxInitPatch;
+				MH_CreateHook(LPVOID(VivoxInitPatch), RetNullptr, 0);
+				Log("Patched: " + std::to_string(VivoxInitPatch - ImageBase) + " with RetNullptr");
+			}
+		}
+
+		auto ConnectRef = Memcury::Scanner::FindStringRef(L"Connect failed: %s (%i)", false);
+		uintptr_t VivoxConnectPatch = ConnectRef.IsValid() ? ConnectRef.FindFunctionStart().Get() : 0;
 		if (VivoxConnectPatch) {
 			MH_CreateHook(LPVOID(VivoxConnectPatch), RetNullptr, 0);
 			Log("Patched: " + std::to_string(VivoxConnectPatch - ImageBase) + " with RetNullptr");
