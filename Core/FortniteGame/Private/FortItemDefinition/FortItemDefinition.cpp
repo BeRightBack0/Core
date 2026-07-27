dd8e944daa2819b46ce4cd378936e00bd6da1250
PongoDev <90353497+PongooDev@users.noreply.github.com>
2026-07-27 17:22:10 +0100

    6.30 support

--- Core/FortniteGame/Private/FortItemDefinition/FortItemDefinition.cpp
+++ Core/FortniteGame/Private/FortItemDefinition/FortItemDefinition.cpp
@@ -40,7 +40,7 @@ int32 UFortItemDefinition::GetMaxStackSize() const
 
 bool UFortItemDefinition::IsStackable() const
 {
-    if (Version::Fortnite_Version <= 6.22)
+    if (Version::Fortnite_Version <= 6.30)
     {
         return GetMaxStackSize() > 1;
     }
